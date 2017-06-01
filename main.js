5d1c0d48e11155a489729771af803e2eeff46f6c
David Wilcox <dave@davidgwilcox.com>
2017-06-02 02:46:22 +1000

    add sub component

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
 'use strict';
 
+var Item = makeComponent({
+	constructor: function() {
+		this.attrs = {
+			kill: this.node.querySelectorAll('span')[0],
+			text: this.node.querySelectorAll('span')[1]
+		};
+	},
+	prototype: {
+		render: function() {
+			this.attrs.text.innerText = this.state;
+		}
+	},
+	events: {
+		'updateText': function(event) {
+			this.state = event.detail;
+			this.render();
+		},
+		'click:.kill-todo': function(event) {
+			event.stopPropagation();
+			this.node.dispatchEvent(new CustomEvent('killTodo', {
+				bubbles: true,
+				detail: this.state
+			}));
+		}
+	}
+});
+
+function makeItemElement() {
+	var li = document.createElement('li');
+	var span = document.createElement('span');
+	span.innerText = 'X';
+	span.classList.add('kill-todo');
+	li.appendChild(span);
+	li.appendChild(document.createElement('span'));
+	return li;
+}
+
 var App = makeComponent({
 	constructor: function() {
 		this.attrs = {
@@ -11,14 +48,12 @@ var App = makeComponent({
 		render: function() {
 			sync(this.attrs.list, this.state, function(li, text) {
 				if (!li) {
-					li = document.createElement('li');
-					var span = document.createElement('span');
-					span.innerText = 'X';
-					span.classList.add('kill-todo');
-					li.appendChild(span);
-					li.appendChild(document.createElement('span'));
+					li = makeItemElement();
+					new Item(li);
 				}
-				li.children[1].innerText = text;
+				li.dispatchEvent(new CustomEvent('updateText', {
+					detail: text
+				}));
 				return li;
 			});
 			return this;
@@ -33,8 +68,8 @@ var App = makeComponent({
 				this.render();
 			}
 		},
-		'click:span.kill-todo': function(event) {
-			var todo = event.target.parentElement.children[1].innerText;
+		'killTodo:li': function(event) {
+			var todo = event.detail;
 			var index = this.state.indexOf(todo);
 			this.state.splice(index, 1);
 			this.render();
